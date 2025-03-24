1dc842530c2f9b7ad050aea42faafd47267c7c45
Josephzino <ewomanzinovictor@gmail.com>
2025-03-24 09:29:43 +0100

    Variable

---
+++ IsAVariable.java
@@ -0,0 +1,12 @@
+public class IsAVariable{
+	public static void main(String[] args){
+		int c, thisIsAVariable, q76354, number;
+		System.out.print("Enter an integer: ");
+		int value = input.nextInt();
+		System.out.println("This is a Java program");
+		System.out.printf("This is a Java program%n", "Java");
+		if (number != 7) System.out.println("The variable number is not equal to 7");
+		
+
+	}
+}
