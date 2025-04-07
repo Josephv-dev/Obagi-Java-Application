a6a784d5623b0e6488ed43c300cf02aa8f43263e
Josephzino <ewomanzinovictor@gmail.com>
2025-04-07 10:00:57 +0100

    Method

---
+++ MethodWithReturnType.java
@@ -0,0 +1,14 @@
+//Instance method//
+public class MethodWithReturnType{
+	public static void main(String[] args){
+		MethodWithReturnType mwrt = new MethodWithReturnType();
+		int result = mwrt.addition(15,30);
+		System.out.printf("%d%n",result);
+		
+	}
+	public int addition(int num1, int num2){
+		int sum = num1 + num2;
+		
+		return sum;
+	}
+}
